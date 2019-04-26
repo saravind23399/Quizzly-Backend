7fe030e10f395753c68df7e8c01cabf58654870f
saravind23399 <saravind23399@gmail.com>
2019-04-27 02:19:46 +0530

    Modified PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyparser = require('body-parser')
 const config = require('./Config/config')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT | 3000
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: true}))
